beb60203cd34612715d331ec304f383ceeb82c00
AKZ-Owner <BBB.azzam.jauzi@gmail.com>
2025-04-27 09:21:36 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>akOne Panel</title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            background-color: #f4f4f9;
+            color: #333;
+            text-align: center;
+            padding: 20px;
+        }
+        .container {
+            background: #fff;
+            border-radius: 8px;
+            box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
+            display: inline-block;
+            padding: 20px 40px;
+            margin-top: 50px;
+        }
+        h1 {
+            color: #007BFF;
+        }
+        p {
+            font-size: 18px;
+        }
+    </style>
+</head>
+<body>
+    <div class="container">
+        <h1>Frontend of akOne</h1>
+        <p>Info PHP: <strong>
+            <?php echo phpversion(); ?>
+        </strong></p>
+        akPanel is a simple and easy to use web panel for your server. It is designed to be lightweight and fast, making it perfect for small to medium-sized servers.
+        <a href="http://akone.test/akp">ke admin panel</a>
+    </div>
+</body>
+</html>
